84c2d7baaed63fb9c8f2a3de28a4d488cdfb88bf
ahtn <jem@seethis.link>
2017-12-30 14:49:28 +1000

    Forgot to add extra row to the keymap

--- keyboards/skyline/keymaps/default/keymap.c
+++ keyboards/skyline/keymaps/default/keymap.c
@@ -42,11 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_0, KC_1, KC_8, KC_9, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_2, KC_3, KC_A, KC_B, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_4, KC_5, KC_C, KC_D, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_6, KC_7, KC_E, KC_F, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
-),
+  KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_0    , KC_1    , KC_8    , KC_9    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC , \
+  KC_ESC  , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_2    , KC_3    , KC_A    , KC_B    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT , \
+  KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_4    , KC_5    , KC_C    , KC_D    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT  , \
+  ADJUST  , KC_LCTL , KC_LALT , KC_LGUI , LOWER   , KC_SPC  , KC_6    , KC_7    , KC_E    , KC_F    , KC_SPC  , RAISE   , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , \
+  S(KC_0) , S(KC_1) , S(KC_2) , S(KC_3) , S(KC_4) , S(KC_5) , S(KC_6) , S(KC_7) , S(KC_8) , S(KC_9) , S(KC_A) , S(KC_B) , S(KC_C) , S(KC_D) , S(KC_E) , S(KC_F)
+)         ,
 
 // TODO Add better layout
 
